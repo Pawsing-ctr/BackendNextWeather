45889ceaa87320198a35f0180d5f3b5ccc7151b4
Pawsing-ctr <tnechayev@internet.ru>
2025-05-01 20:15:04 +0300

    fix

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://backendnextweather-production.up.railway.app"],
+    origin: ["https://nextweather.railway.internal"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
