f29c994a48e704fa3d700560b7a8dd4537529b35
Pawsing-ctr <tnechayev@internet.ru>
2025-05-08 17:54:45 +0300

    fix

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://next-weather-delta.vercel.app/"],
+    origin: ["https://next-weather-delta.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
